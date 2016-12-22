5b24e2865784d4824108e032e2a8e94f78ad192e
nyayaya <nyayaya@users.noreply.github.com>
2016-12-22 22:43:28 +0000

    Add files via upload

---
+++ Steam auto queue script.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Steam auto queue script
+// @version      1.00
+// @author       nyayaya
+// @description  i think it works
+// @include      http://store.steampowered.com/*
+// @include      https://store.steampowered.com/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+var curUrl = window.location.pathname;
+
+if(curUrl.indexOf("explore") != -1){
+    var temp = document.getElementsByClassName("discovery_queue_winter_sale_cards_header")[0].getElementsByClassName("subtext")[0].innerHTML;
+    alert(temp);
+    if(temp.indexOf("Come back") == -1){
+        document.getElementById("refresh_queue_btn").click();
+    }
+}
+else{
+    document.getElementById("next_in_queue_form").submit();
+}
